2ca211342a1cfea3984acffaa5d584520b7a7bbe
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2010-03-15 04:59:27 +0900

    fix integer size

--- src/types.h
+++ src/types.h
@@ -1,15 +1,15 @@
-// サイズ指定の型の定義
-// 環境によって変える必要があるかも知れず
 #ifndef _TYPES_H
 #define _TYPES_H
 
-typedef char           s8;
-typedef unsigned char  u8;
-typedef short          s16;
-typedef unsigned short u16;
-typedef long           s32;
-typedef unsigned long  u32;
-typedef long long      s64;
-typedef unsigned long long u64;
+#include <stdint.h>
+
+typedef int8_t   s8;
+typedef uint8_t  u8;
+typedef int16_t  s16;
+typedef uint16_t u16;
+typedef int32_t  s32;
+typedef uint32_t u32;
+typedef int64_t  s64;
+typedef uint64_t u64;
 
 #endif
